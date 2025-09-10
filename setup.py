3e63d4f629f455087b644e69e6db187c640b152e
ManoFayyaz <mehnoorfayyaz06@gmail.com>
2025-09-10 19:54:19 +0500

    installed requirements

--- setup.py
+++ setup.py
@@ -1,1 +1,18 @@
 #local packages in my environment
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mcq_genai",
+    version="0.1.0",
+    author="Mehnoor Fayyaz",
+    author_email="mehnoorfayyaz06@gmail.com",
+    install_requires=[
+        "openai",
+        "python-dotenv",
+        "streamlit",
+        "langchain",
+        "PyPDF2"
+    ],
+    packages=find_packages()
+)
