08a823a9642695db6bc6362e7a71b45c40fe300d
ManoFayyaz <mehnoorfayyaz06@gmail.com>
2026-06-14 02:00:12 +0500

    google o auth in index js

--- frontend/quiz_app/src/index.js
+++ frontend/quiz_app/src/index.js
@@ -8,13 +8,13 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+        <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
-<GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
-  <App />
-</GoogleOAuthProvider>
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
