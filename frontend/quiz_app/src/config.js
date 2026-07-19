63e9590ed66557a5741310ddcf47a4adaec70a84
ManoFayyaz <mehnoorfayyaz06@gmail.com>
2026-07-19 23:26:31 +0500

    fixed the url in config.js

--- frontend/quiz_app/src/config.js
+++ frontend/quiz_app/src/config.js
@@ -1,3 +1,3 @@
 // const API_URL = "https://mcq-genai-production-93ce.up.railway.app";
-const API_URL = "mcq-genai-production.up.railway.app";
+const API_URL = "https://mcq-genai-production.up.railway.app";
 export default API_URL;
